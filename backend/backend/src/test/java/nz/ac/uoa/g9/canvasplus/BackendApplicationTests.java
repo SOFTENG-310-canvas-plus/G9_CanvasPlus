beab1f72ace995eb74e5cff0b1bb9fa85b8cd1a5
ZingZing_ <rzha972@aucklanduni.ac.nz>
2025-08-13 20:00:09 +1200

    Initialized backend for OPENAI API testing, and established database connection strings

---
+++ backend/backend/src/test/java/nz/ac/uoa/g9/canvasplus/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.ac.uoa.g9.canvasplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

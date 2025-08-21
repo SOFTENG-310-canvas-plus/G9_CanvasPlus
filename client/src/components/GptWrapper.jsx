5ace983f2f94638874e45a4845d378b5c7240fb0
ZingZing_ <rzha972@aucklanduni.ac.nz>
2025-08-21 20:01:05 +1200

    Add CSS import to GptWrapper for improved styling

--- client/src/components/GptWrapper.jsx
+++ client/src/components/GptWrapper.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import "../App.css"
 
 // CHANGE: Added helper to pretty-print JSON and copy to clipboard
 const stringifyPretty = (obj) => {

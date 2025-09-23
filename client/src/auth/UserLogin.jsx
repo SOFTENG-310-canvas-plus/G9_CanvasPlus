dc1a79e9b5e00db07c8e6e65540ef359353eea8e
Dave <dkha055@aucklanduni.ac.nz>
2025-09-23 14:55:26 +1200

    feat(user authentication): Created function to retirve the logged in users id, to be used in other components

--- client/src/auth/UserLogin.jsx
+++ client/src/auth/UserLogin.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import supabase from "./supabaseClient.js";
+import {supabase} from "./supabaseClient.js";
 import { toast, Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
--- client/src/auth/supabaseClient.js
+++ client/src/auth/supabaseClient.js
@@ -2,6 +2,11 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ljygydqwyhlsxhwtnuiz.supabase.co'
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY || ''
-const supabase = createClient(supabaseUrl, supabaseKey)
 
-export default supabase;
+export const supabase =  createClient(supabaseUrl, supabaseKey)
+
+export async function getUserId() {
+    const { data: { user } } = await supabase.auth.getUser();
+    return user?.id;
+}
+

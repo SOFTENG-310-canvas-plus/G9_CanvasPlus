f2474d50393c14214107127a2504fbe3896b86d4
Dave <dkha055@aucklanduni.ac.nz>
2025-10-04 22:01:50 +1300

    feat(user authentication): Fixing authentication test to make them pass and more robust

--- client/src/auth/UserLogin.jsx
+++ client/src/auth/UserLogin.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+
+import React, { useState } from "react";
 import {supabase} from "./supabaseClient.js";
 import { toast, Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
